aa82aa714b11005d0dae838284e05c00131a4344
kad <footnofetish123@gmail.com>
2024-08-11 19:36:48 -0400

    chore: Update package.json and postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -4,4 +4,30 @@ module.exports = {
     tailwindcss: {},
     autoprefixer: {},
   },
+  "name": "whisker-wisdom",
+  "version": "1.0.0",
+  "scripts": {
+    "build": "next build",
+    "start": "next start",
+    "dev": "next dev",
+    "lint": "next lint",
+    "export": "next export" // Add this line if the export script is missing
+  },
+  "dependencies": {
+    "next": "14.2.5",
+    "react": "18.2.0",
+    "react-dom": "18.2.0"
+  },
+  "devDependencies": {
+    "autoprefixer": "^10.4.0",
+    "postcss": "^8.4.0",
+    "tailwindcss": "^2.2.19"
+  },
+  "scripts": {
+    "build": "next build",
+    "start": "next start",
+    "dev": "next dev",
+    "lint": "next lint",
+    "export": "next export"
+  }
 };
